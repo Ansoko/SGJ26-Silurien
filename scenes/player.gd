09c86c29b879a57bfb796888234c43c9476c2af2
AliceMassart <lissounem@gmail.com>
2026-03-14 10:23:43 +0100

    Sprite Jump + fix issues
    Instead of being a walk sprite for the jump action, I modified the conditions in player.gd to have the jump sprite.
    
    + fixed tilemap issues

--- scenes/player.gd
+++ scenes/player.gd
@@ -12,26 +12,20 @@ func _ready():
 
 func _physics_process(delta):
 
-	# Mouvement horizontal
 	var direction = 0
 	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_POWER
+		
 	if Input.is_action_pressed("move_right"):
 		direction += 1
 	if Input.is_action_pressed("move_left"):
 		direction -= 1
 
 	velocity.x = direction * speed
-
-	# Gravité
 	velocity.y += GRAVITY * delta
 
-	# Saut
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_POWER
-
-	# Applique le mouvement
 	move_and_slide()
-
 	update_animation()
 
 
@@ -42,7 +36,7 @@ func update_animation():
 	else:
 		$AnimatedSprite2D.stop()
 
-	if velocity.x != 0:
+	if velocity.x != 0 and velocity.y == 0:
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 
