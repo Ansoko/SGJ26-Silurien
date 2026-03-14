11d74f1b1d54a409851dffa96ef8b4667013e552
AliceMassart <lissounem@gmail.com>
2026-03-14 15:44:30 +0100

    timer changed

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,10 +4,18 @@ extends Node2D
 
 func _ready():
 	SignalManager.start_game.connect(start_timer)
+	$Texts/Timer_label.hide()
 
 func start_timer():
+	$Texts/Timer_label.show()
 	timer.start()
 	print("Timer démarré")
 	
+	
 func _on_timer_timeout():
 	get_tree().change_scene_to_file("res://scenes/end_screen.tscn")
+
+func _process(delta):
+	if !timer.is_stopped():
+		$Texts/Timer_label.text = str(int(timer.get_time_left())) + "s left."
+	
